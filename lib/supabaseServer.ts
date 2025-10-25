16d60d47f9b9ae9812c2f70141faff4bfc94b21d
nmazzaroni <nmazzaroni@gmail.com>
2025-10-25 01:35:01 -0300

    fix: reintroducción de cookies con tipado temporal

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -5,7 +5,10 @@ import { createClient } from "@supabase/supabase-js";
 export function supabaseServer() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: cookies() as any // tipado temporal para validar funcionalidad
+    }
   );
 }
 
