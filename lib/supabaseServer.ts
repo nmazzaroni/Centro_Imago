639a5a42d2dc851260e59125b055ec20b4643c43
nmazzaroni <nmazzaroni@gmail.com>
2025-10-25 01:32:53 -0300

    fix: version finalcompatible de supabaseServer

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -3,14 +3,9 @@ import { createServerClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
 
 export function supabaseServer() {
-  const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: cookieStore
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
 
